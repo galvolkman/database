ca4fc12cc7f31e758d206c598bfefdc513acdfdd
galvolkman <gal.volkman@gmail.com>
2023-11-09 12:49:02 +0200

    phase 2 with test procedures done.

--- database.py
+++ database.py
@@ -20,10 +20,15 @@ class Database:
             return self.dict.pop(key)
         return None
 
+    def __str__(self):
+        return self.dict.__str__()
+
 
 class DictManager(Database):
     def __init__(self):
         super().__init__()
+        f = open("database.json", "r")
+        self.dict = json.load(f)
 
     def set_value(self, key, val):
         super().set_value(key, val)
@@ -36,5 +41,25 @@ class DictManager(Database):
         f = open("database.json", "w")
         json.dump(self.dict, f)
 
+def main1():
+    mydb = Database()
+    mydb.set_value("kuku", "123456")
+    print(mydb.get_value("gal"))
+    print(mydb.get_value("eli"))
+    print(mydb)
 
 
+def main2():
+    mydb = DictManager()
+    mydb.set_value("kuku", "123456")
+    mydb.set_value("gal", "123456")
+
+    print(mydb.get_value("gal"))
+    print(mydb.get_value("eli"))
+    print(mydb)
+
+
+if __name__ == "__main__":
+    #main1()
+    main2()
+    #main3()
